6f231582f35aa8185c603ca139743c53a9e3d6f7
Martin Fahy <martinfahy.mdf@gmail.com>
2025-03-28 08:56:45 +1000

    implemented add function for learning management system courses

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,11 +1,24 @@
-// get task (read)
-const Task = require('../models/Task');
-const getTasks = async (
+// get course (read)
+const Course = require('../models/Course');
+const getCourses = async (
 req,
 res) => {
 try {
-const tasks = await Task.find({ userId: req.user.id });
-res.json(tasks);
+const courses = await Course.find({ userId: req.user.id });
+res.json(courses);
 } catch (error) {
 res.status(500).json({ message: error.message });
 }}
+
+// add course
+const addCourse = async (
+    req,
+    res) => {
+    const { title, description, deadline } = req.body;
+    try {
+    const course = await Course.create({ userId: req.user.id, title, description, deadline });
+    res.status(201).json(course);
+    } catch (error) {
+    res.status(500).json({ message: error.message });
+    }
+    };
